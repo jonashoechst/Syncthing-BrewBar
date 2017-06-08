d161cabfae054eaa52cce850fa8e34a2ee794ba2
Jonas Höchst <git@jonashoechst.de>
2017-06-08 22:41:16 +0200

    Initial Commit

---
+++ syncthing-brew-gui/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  syncthing-brew-gui
+//
+//  Created by Jonas Höchst on 08.06.17.
+//  Copyright © 2017 Jonas Höchst. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ syncthing-brew-gui/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  syncthing-brew-gui
+//
+//  Created by Jonas Höchst on 08.06.17.
+//  Copyright © 2017 Jonas Höchst. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
